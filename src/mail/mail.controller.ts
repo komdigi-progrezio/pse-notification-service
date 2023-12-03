8382b821ba3ac174371f00837dfec08f5daf816d
Alexander Kevin <alexander.kevin@shortlyst.ai>
2023-12-03 16:13:45 +0700

    Add more mail
    - pejabatPendaftarBaru
    - pejabatPendaftarPengganti
    - pendaftaranSubPejabat
    - pendaftaranSeBaru
    - pendaftaranSe100

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -11,4 +11,29 @@ export class MailController {
     return this.mailService.pejabatPendaftarAktivasi(request);
   }
 
+  @MessagePattern('pejabatPendaftarBaru')
+  pejabatPendaftarBaru(@Payload() request: any) {
+    return this.mailService.pejabatPendaftarBaru(request);
+  }
+
+  @MessagePattern('pejabatPendaftarPengganti')
+  pejabatPendaftarPengganti(@Payload() request: any) {
+    return this.mailService.pejabatPendaftarPengganti(request);
+  }
+  
+  @MessagePattern('pendaftaranSubPejabat')
+  pendaftaranSubPejabat(@Payload() request: any) {
+    return this.mailService.pendaftaranSubPejabat(request);
+  }
+  
+  @MessagePattern('pendaftaranSeBaru')
+  pendaftaranSeBaru(@Payload() request: any) {
+    return this.mailService.pendaftaranSeBaru(request);
+  }
+  
+  @MessagePattern('pendaftaranSe100')
+  pendaftaranSe100(@Payload() request: any) {
+    return this.mailService.pendaftaranSe100(request);
+  }
+
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -17,4 +17,60 @@ export class MailService {
 
     return data;
   }
+
+  async pejabatPendaftarBaru(user: any): Promise<void> {
+    return await this.mailerService.sendMail({
+      to: 'pse@layanan.go.id',
+      subject: 'Pejabat Pendaftar baru telah melakukan pendaftaran',
+      template: 'pejabat_pendaftar_baru',
+      context: {
+        user,
+      },
+    });
+  }
+  
+  async pejabatPendaftarPengganti(user: any): Promise<void> {
+    return await this.mailerService.sendMail({
+      to: 'pse@layanan.go.id',
+      subject: 'Pejabat Pengganti baru telah melakukan pendaftaran',
+      template: 'pejabat_pendaftar_pengganti',
+      context: {
+        user,
+      },
+    });
+  }
+
+  async pendaftaranSubPejabat(user: any): Promise<void> {
+    return await this.mailerService.sendMail({
+      to: 'pse@layanan.go.id',
+      subject: 'Sub-Pejabat baru telah didaftarkan',
+      template: 'pendaftaran_sub_pejabat',
+      context: {
+        user,
+      },
+    });
+  }
+  
+  async pendaftaranSeBaru(data: any): Promise<void> {
+    return await this.mailerService.sendMail({
+      to: 'pse@layanan.go.id',
+      subject: 'Sistem Elektronik baru telah didaftarkan',
+      template: 'pendaftaran_se_baru',
+      context: {
+        data,
+      },
+    });
+  }
+
+  async pendaftaranSe100(user: any): Promise<void> {
+    return await this.mailerService.sendMail({
+      to: '',
+      subject: 'Sistem Elektronik berikut telah mencapai kelengkapan data 100%',
+      template: 'pendaftaran_se_100',
+      context: {
+        user,
+      },
+    });
+  }
+
 }
