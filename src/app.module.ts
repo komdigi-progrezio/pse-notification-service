b446d66788db6d1cf1f4969385c73d58dfc5cd4b
Arief <arief.hendrawan@deltadatamandiri.comeslint>
2024-05-14 01:39:55 +0700

    Optimize exessive logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,6 +45,7 @@ import { MailModule } from './mail/mail.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      logging: false,
       models: [],
       autoLoadModels: true,
       synchronize: true,
